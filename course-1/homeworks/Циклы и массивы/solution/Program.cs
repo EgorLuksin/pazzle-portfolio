3afbcb574f55e776c7011b0a6c971540c6e4a30e
EgorLukshin <egorlukshin25@gmail.com>
2025-12-02 21:20:54 +0300

    feat: Добавлены папки для практики 4 урока и домашнего задания на циклы и массивы в первом курсе

---
+++ course-1/homeworks/Циклы и массивы/solution/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Задание 1:");
+        int[] a = { 3, 4, 5, 2, 4, 3, 5 };
+        double count = 0;
+        int five = 0;
+        int two = 0;
+
+        foreach (int i in a)
+        {
+            if (i == 2)
+            {
+                two++; 
+            } 
+            else if (i == 5)
+            {
+                five++;
+            }
+
+            count += i;
+        }
+        Console.WriteLine("Средний балл: " + (count / a.Length));
+        Console.WriteLine("Количество двоек: " + two);
+        Console.WriteLine("Колличество пятёрок: " + five);
+
+
+        Console.WriteLine();
+        Console.WriteLine("Задание 2:");
+
+        int[] b = { 1, 2, 3, 4, 5 };
+        int len = b.Length;
+        int[] c = new int[len];
+        for (int i = 0; i < len; i ++)
+        {
+            Console.Write(b[i] + " ");
+            c[len - i - 1] = b[i];
+        }
+        Console.WriteLine();
+        foreach (int i in c)
+        {
+            Console.Write(i + " ");
+        }
+
+        Console.WriteLine();
+        Console.WriteLine();
+
+        for (int i = 0; i < len; i++)
+        {
+            Console.Write(b[i] + " ");
+        }
+        Console.WriteLine();
+        for (int i = len - 1; i >= 0; i--)
+        {
+            Console.Write(b[i] + " ");
+        }
+
+
+        Console.WriteLine();
+        Console.WriteLine("Задание 3:");
+
+        for (int i = 1; i <= 20; i++)
+        {
+            Console.Write(i + " ");
+        }
+        Console.WriteLine();
+        for (int i = 1; i <= 20; i++)
+        {   
+            if (i % 2 == 1)
+            {
+                continue;
+            }
+            Console.Write(i + " ");
+        }
+        Console.WriteLine();
+
+
+        Console.WriteLine();
+        Console.WriteLine("Задание 4:");
+
+        string pasword = "1234";
+
+        for (int i = 0; i < 4; i++)
+        {
+            if (i == 3)
+            {
+                Console.WriteLine("Доступ заблокирован!");
+                break;
+            }
+            Console.Write("Введите пароль: ");
+            if (Console.ReadLine() == pasword)
+            {
+                Console.WriteLine("Доступ получен!"); 
+                break; 
+            }
+            Console.WriteLine("Пароль неверный!");
+            Console.WriteLine();
+        }
+    }
+}
---
+++ course-1/practices/lesson_4/solution/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Задание 1:");
+        int[] a = { 1, 2, 3, 4, 5 };
+        for (int i = 0; i < a.Length; i ++)
+        {
+            Console.WriteLine(a[i]);
+        }
+
+
+        Console.WriteLine();
+        Console.WriteLine("Задание 2:");
+
+        double b = 0;
+        a = new int[] { 3, 2 ,5 ,4 ,5 };
+        for (int i = 0; i < a.Length; i ++)
+        {
+            b += a[i];
+        }
+        b /= a.Length;
+        Console.WriteLine(b);
+
+
+        Console.WriteLine();
+        Console.WriteLine("Задание 3:");
+
+        int[] c = { 45, 1, 89, 32, 56, 18, 76, 24 };
+        int d = 0;
+        foreach (int i in c)
+        {
+            if (i > d)
+            {
+                d = i;
+            }
+        }
+        Console.WriteLine(d);
+
+
+        Console.WriteLine();
+        Console.WriteLine("Задание 4:");
+
+        string[] fruit = { "Яблоко", "Груша", "Персик", "Абрикос", "Киви" };
+        foreach (string s in fruit)
+        {
+            Console.Write(s + ", "); 
+        }
+        Console.WriteLine();
+
+
+        Console.WriteLine();
+        Console.WriteLine("Задание 5:");
+
+        int[,] f =
+        {
+            {1, 2, 3 },
+            {4, 5, 6 },
+            {7, 8, 9 }
+        };
+
+        for (int i = 0; i < f.GetLength(0); i++)
+        {
+            for (int j = 0; j < f.GetLength(1); j++)
+            {
+                Console.Write(f[i, j] + " ");
+            }
+            Console.WriteLine("");
+        }
+
+
+        Console.WriteLine();
+        Console.WriteLine("Задание 6:");
+
+        string str;
+        do
+        {
+            Console.WriteLine("Введите пароль: ");
+            str = Console.ReadLine();
+            if (str == "1234")
+            {
+                Console.WriteLine("Доступ получен!");
+                break;
+            }
+            Console.WriteLine("Пароль неверный!");
+            Console.WriteLine("");
+        } while (true);
+    }
+}
